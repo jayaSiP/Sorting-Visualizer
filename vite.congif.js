6bfbedb34c922647b3e3fee88044fd39a7cf9d95
jayaSiP <22052821kiit@gmail.com>
2025-06-01 05:18:44 +0530

    fixing

--- vite.congif.js
+++ vite.congif.js
@@ -1,24 +1,8 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default {
     plugins: [react()],
     optimizeDeps: {
-        include: [
-            '@mui/material',
-            '@mui/system',
-            '@mui/styled-engine',
-            '@mui/utils',
-            // Add other MUI packages you use
-        ],
+        include: ['@mui/material', '@mui/system', '@mui/icons-material'],
     },
-    ssr: {
-        noExternal: [
-            '@mui/material',
-            '@mui/system',
-            '@mui/styled-engine',
-            '@mui/utils',
-            // same packages here for SSR build
-        ],
-    },
-});
+};
