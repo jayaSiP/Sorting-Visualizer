aa4e3f1ae6f1dbca60538f1c51f03feb45f4f0eb
jayaSiP <22052821kiit@gmail.com>
2025-06-01 04:11:09 +0530

    Update index.js to use React 18 createRoot method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import SortingVisualizer from "./SortingVisualizer/SortingVisualizer.jsx";
 
-// ## Rendering SortingVisualizer Component ( Imported from path mentioned above ) ## //
-ReactDOM.render(<SortingVisualizer />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<SortingVisualizer />);
